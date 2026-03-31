19f828b2c98982d11b103eaf5bcc12d429f99158
Alex <a.kolodin13@gmail.com>
2026-03-31 23:25:06 +0300

    Homework №10

---
+++ Homework10.1.py
@@ -0,0 +1,22 @@
+        # 10.1. Генераторна функція
+
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    """
+     begin: перший елемент послідовності
+     end: кількість елементів у послідовності
+     func: функція, яка формує значення для послідовності
+    """
+    count = begin
+    for i in range(end):
+        yield count
+        count = func(count)
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
---
+++ Homework10.3.py
@@ -0,0 +1,25 @@
+        # 10.3. Перевірити чи є парним чи ні
+
+def is_even(digit):
+    """ Перевірка чи є парним число """
+
+
+    return digit % 2 == 0
+
+
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
+print('OK')
+
+  ########################
+
+        # if digit % 2 == 0:
+        #     return True
+        # else:
+        #     return False
+
+
+        # if digit % 2 == 0:
+        #     return True
+        # return False
